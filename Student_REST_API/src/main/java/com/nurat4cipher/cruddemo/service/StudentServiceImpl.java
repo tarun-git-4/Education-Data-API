2c3ff5a710db52e1030d1fbf2b98879a390c5718
Tarun Jayadevan <100798323+tarun-git-4@users.noreply.github.com>
2023-07-16 01:12:03 +0530

    Add files via upload

---
+++ Student_REST_API/src/main/java/com/nurat4cipher/cruddemo/service/StudentServiceImpl.java
@@ -0,0 +1,45 @@
+package com.nurat4cipher.cruddemo.service;
+
+import com.nurat4cipher.cruddemo.dao.StudentRepository;
+import com.nurat4cipher.cruddemo.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        Optional<Student> result = studentRepository.findById(id);
+        Student student = null;
+        if(result.isPresent()){
+            student = result.get();
+        }
+        return student;
+    }
+
+    @Override
+    public Student save(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        studentRepository.deleteById(id);
+    }
+}
