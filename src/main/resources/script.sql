9f9c74474a488e1afb5c3b25a7f6da68b351fd72
Tarun Jayadevan <100798323+tarun-git-4@users.noreply.github.com>
2023-09-01 08:52:42 +0530

    Update script.sql

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,11 +1,7 @@
 CREATE DATABASE  IF NOT EXISTS `student_directory`;
 USE `student_directory`;
 
---
--- Table structure for table `employee`
---
 
-# DROP TABLE IF EXISTS `employee`;
 
 CREATE TABLE `student` (
                            `id` int NOT NULL AUTO_INCREMENT,
@@ -15,9 +11,6 @@ CREATE TABLE `student` (
                            PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
---
--- Data for table `employee`
---
 
 INSERT INTO `student` VALUES
                           (1,'Tarun','Jayadevan','tarunjayan4@gmail.com'),
